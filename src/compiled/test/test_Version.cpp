c59dfebac2615dbabf2aed0fd3a7ac7884ccdc4b
bareya <piotrbarejko@gmail.com>
2019-11-27 19:36:29 -0800

    Token update

--- src/compiled/test/test_Version.cpp
+++ src/compiled/test/test_Version.cpp
@@ -2,20 +2,73 @@
 
 #include "../version/Version.hpp"
 
-TEST(Version, Initialization)
+#include <tuple>
+
+//
+// Numeric Version
+//
+class NumericVersionThrowTest : public ::testing::TestWithParam<string_view>{};
+TEST_P(NumericVersionThrowTest, InitThrow)
 {
-    auto a = Factory<AlphanumericVersion>::Create("1.2.3");
-    auto r = Factory<ReversedAlphanumericVersion>::Create("1.2.4");
+    EXPECT_THROW(Factory<NumericVersion>::Create(GetParam()), VersionError);
+}
+INSTANTIATE_TEST_SUITE_P(InitThrow, NumericVersionThrowTest,
+                         testing::Values(
+                             " ", ".", "-", "--", "..", "!", ">", ".10.", ".10", "10.", "abc.1"
+                         ));
 
-    ASSERT_TRUE(a < r);
-    ASSERT_TRUE( r < a );
+class NumericVersionNoThrowTest : public ::testing::TestWithParam<string_view>{};
+TEST_P(NumericVersionNoThrowTest, InitNoThrow)
+{
+    EXPECT_NO_THROW(Factory<AlphanumericVersion>::Create(GetParam()));
+}
+INSTANTIATE_TEST_SUITE_P(InitNoThrow, NumericVersionNoThrowTest,
+                         testing::Values(
+                             "", "0.1-10", "1.2.3", "1-2-3"
+                         ));
 
-    ASSERT_EQ(a->Size(), 3);
+//
+// Alphanumeric Version
+//
 
-    ASSERT_EQ(a->Major(), "1"_at);
-    ASSERT_EQ(a->Minor(), "2"_at);
-    ASSERT_EQ(a->Patch(), "3"_at);
+class AlphanumericVersionThrowTest : public ::testing::TestWithParam<string_view>{};
+TEST_P(AlphanumericVersionThrowTest, InitThrow)
+{
+    EXPECT_THROW(Factory<AlphanumericVersion>::Create(GetParam()), VersionError);
+}
+INSTANTIATE_TEST_SUITE_P(InitThrow, AlphanumericVersionThrowTest,
+                         testing::Values(
+                             " ", ".", "-", "--", "..", "!", ">", ".10.", ".10", "10."
+                         ));
 
-    ASSERT_EQ(a->seps[0], ".");
-    ASSERT_EQ(a->seps[1], ".");
+class AlphanumericVersionNoThrowTest : public ::testing::TestWithParam<string_view>{};
+TEST_P(AlphanumericVersionNoThrowTest, InitNoThrow)
+{
+    EXPECT_NO_THROW(Factory<AlphanumericVersion>::Create(GetParam()));
+}
+INSTANTIATE_TEST_SUITE_P(InitNoThrow, AlphanumericVersionNoThrowTest,
+                         testing::Values(
+                             "", "foo", "1_a", "0.1-10", "1.2.3", "1-2-3", "foo-bar", "foo_bar"
+                         ));
+
+class AlphanumericVersionTokenCountTest : public ::testing::TestWithParam<std::pair<string_view, rez_int>>{};
+TEST_P(AlphanumericVersionTokenCountTest, TokenCount)
+{
+    ASSERT_EQ(Factory<AlphanumericVersion>::Create(GetParam().first)->size(), GetParam().second);
+}
+INSTANTIATE_TEST_SUITE_P(TokenCount, AlphanumericVersionTokenCountTest,
+                         testing::Values(
+                             std::make_pair("", 0),
+                             std::make_pair("1", 1),
+                             std::make_pair("foo_bar", 1),
+                             std::make_pair("1-2", 2),
+                             std::make_pair("1.2", 2),
+                             std::make_pair("1.2-3", 3),
+                             std::make_pair("foo1.2bar-3world5", 3)
+                         ));
+
+TEST(A, B)
+{
+    auto v = Factory<NumericVersion>::Create("10");
+    std::cout << v.Hash() << std::endl;
 }
--- src/compiled/version/Token.hpp
+++ src/compiled/version/Token.hpp
@@ -1,152 +1,95 @@
 #ifndef REZ_TOKEN_HPP
 #define REZ_TOKEN_HPP
 
-#include "Comparable.hpp"
+#include "Types.hpp"
 
 #include <regex>
 
-template<typename _Typ> struct TokenData
-{
-    using value_type = _Typ;
-
-    // data access
-    const value_type& Get() const REZ_NOEXCEPT { return _value; }
-    value_type& Get()  REZ_NOEXCEPT { return _value; }
-
-    // operator->
-    const value_type* operator->() const REZ_NOEXCEPT { return &Get; }
-    value_type* operator->() REZ_NOEXCEPT { return &Get(); }
-
-    // operator*
-    const value_type& operator*() const REZ_NOEXCEPT { return Get(); }
-    value_type& operator*() REZ_NOEXCEPT { return Get(); }
-
-    // operator<
-    bool operator<(const TokenData<value_type>& other) const REZ_NOEXCEPT
-    {
-        return _value < other.Get();
-    }
-
-    // operator<
-    bool operator<(const value_type& other) const REZ_NOEXCEPT
-    {
-        return _value < other;
-    }
-
-    // operator==
-    bool operator==(const TokenData<value_type>& other) const REZ_NOEXCEPT
-    {
-        return _value == other.Get();
-    }
-
-    // operator==
-    bool operator==(const value_type& other) const REZ_NOEXCEPT
-    {
-        return _value == other;
-    }
-
-    value_type _value;
-};
-
-template<typename T> class SubToken
+//
+//
+//
+class SubToken
 {
 public:
-    explicit SubToken(const T& v)
+    using value_type = string_view;
+
+    explicit SubToken(const value_type& v) REZ_NOEXCEPT
     {
         s = v;
         n = is_digit(s) ? to_int(s) : REZ_INT_INVALID;
     }
 
-    explicit SubToken(T&& v)
+    explicit SubToken(value_type&& v) REZ_NOEXCEPT
     {
         s = std::move(v);
         n = is_digit(s) ? to_int(s) : REZ_INT_INVALID;
     }
 
-    // operator<
     bool operator<(const SubToken& other) const REZ_NOEXCEPT
     {
         if (n == REZ_INT_INVALID)
         {
             return other.n == REZ_INT_INVALID ? s < other.s : true;
         }
-
         return other.n == REZ_INT_INVALID ? false : std::tie(n, s) < std::tie(other.n, other.s);
     }
 
-    // operator<
-    bool operator<(rez_int other) const REZ_NOEXCEPT
-    {
-        return other == REZ_INT_INVALID ? false : n < other;
-    }
+    bool operator<(rez_int other) const REZ_NOEXCEPT { return other == REZ_INT_INVALID ? false : n < other; }
 
-    // operator==
-    bool operator==(const SubToken& other) const REZ_NOEXCEPT
-    {
-        return (s == other.s) && (n == other.n);
-    }
+    bool operator==(const SubToken& other) const REZ_NOEXCEPT { return (s == other.s) && (n == other.n); }
+    bool operator==(rez_int other) const REZ_NOEXCEPT { return other == REZ_INT_INVALID ? false : n == other; }
 
-    // operator==
-    bool operator==(rez_int other) const REZ_NOEXCEPT
-    {
-        return other == REZ_INT_INVALID ? false : n == other;
-    }
-
-    T s;
+    value_type s;
     rez_int n;
 };
 
-// is_token type
-template<typename _Typ> struct is_token : std::false_type
+//
+// Token Aliases
+//
+
+using NumericToken = rez_int;
+using AlphanumericToken = std::vector<SubToken>;
+
+//
+// To string conversion
+//
+
+inline std::string to_string(const NumericToken& other) { return std::to_string(other); }
+
+inline std::string to_string(const AlphanumericToken& other)
 {
-};
-
-template<typename _Typ, bool _Rev> struct is_token<Comparable<TokenData<_Typ>, _Rev>> : std::true_type
-{
-};
-
-//
-// Numeric Token
-//
-using NumericValue = rez_int;
-template<bool _Rev> using NumericTokenT = Comparable<TokenData<NumericValue>, _Rev>;
-
-using NumericToken = NumericTokenT<NORMAL>;
-using ReversedNumericToken = NumericTokenT<REVERSED>;
-
-//
-// Alphanumeric Token
-//
-using AlphanumericSubToken = SubToken<string_view>;
-using AlphanumericValue = std::vector<AlphanumericSubToken>;
-
-template<bool _Rev> using AlphanumericTokenT = Comparable<TokenData<AlphanumericValue>, _Rev>;
-using AlphanumericToken = AlphanumericTokenT<NORMAL>;
-using ReversedAlphanumericToken = AlphanumericTokenT<REVERSED>;
-
-//
-// Factory specializations
-//
-
-template<bool _Rev> struct Factory<NumericTokenT<_Rev>>
-{
-    static_assert(is_comparable<NumericToken>::value, "Invalid template parameter, expected Comparable!");
-    using value_type = NumericToken::value_type;
-
-    static Comparable<value_type, _Rev> Create(string_view token)
+    std::string str;
+    for (const auto& sub_token : other)
     {
-        return Comparable<value_type, _Rev>{to_int(token)};
+        str.append(sub_token.s.cbegin(), sub_token.s.cend());
+    }
+    return str;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const AlphanumericToken& other) { return (os << to_string(other)); }
+
+//
+// Token Factory
+//
+
+template <> struct Factory<NumericToken>
+{
+    static NumericToken Create(string_view token)
+    {
+        if (token.empty())
+        {
+            throw std::invalid_argument(std::string{"Invalid string to parse: "} + token.to_string());
+        }
+
+        return to_int(token);
     }
 };
 
-template<bool _Rev> struct Factory<AlphanumericTokenT<_Rev>>
+template <> struct Factory<AlphanumericToken>
 {
-    static_assert(is_comparable<AlphanumericToken>::value, "Invalid template parameter, expected Comparable!");
-    using value_type = AlphanumericToken::value_type;
-
-    static Comparable<value_type, _Rev> Create(string_view token)
+    static AlphanumericToken Create(string_view token)
     {
-        if (token.empty() && !is_alphanumeric(token))
+        if (token.empty() || !is_alphanumeric(token))
         {
             std::string message;
             message.append("Invalid string to parse ");
@@ -154,21 +97,22 @@ template<bool _Rev> struct Factory<AlphanumericTokenT<_Rev>>
             throw std::runtime_error(message);
         }
 
-        string_view cached_token = token;
+        // TODO: make more generic it work with string. string_view
+        SubToken::value_type cached_token = token;
 
         // split by numbers
         static const std::regex n_regex{R"([0-9]+)", std::regex::optimize};
 
-        std::regex_iterator<string_view::const_iterator> it{cached_token.cbegin(), cached_token.cend(), n_regex};
-        std::regex_iterator<string_view::const_iterator> last{};
+        std::regex_iterator<SubToken::value_type::const_iterator> it{cached_token.cbegin(), cached_token.cend(), n_regex};
+        std::regex_iterator<SubToken::value_type::const_iterator> last{};
 
-        typename AlphanumericTokenT<_Rev>::value_type tokens;
+        AlphanumericToken tokens;
 
         // no numbers
         if (it == last)
         {
-            tokens->emplace_back(cached_token);
-            return AlphanumericTokenT<_Rev>{std::move(tokens)};
+            tokens.emplace_back(std::move(cached_token));
+            return {tokens};
         }
 
         // iterate number splits
@@ -177,69 +121,35 @@ template<bool _Rev> struct Factory<AlphanumericTokenT<_Rev>>
             // convert to string value: 12_hello_world_34 -> [12(int), "_hello_world_"(str), 34(int)]
             if (it->prefix().length())
             {
-                const std::cmatch::value_type& match = it->prefix();
-                tokens->emplace_back(string_view{match.first, static_cast<size_t>(match.length())});
+                // TODO: make more generic it work with string. string_view
+                const auto& match = it->prefix();
+                tokens.emplace_back(SubToken::value_type{match.first, static_cast<size_t>(match.length())});
             }
 
-            // convert to int value
-            const std::cmatch::value_type& match = (*it)[0];
-            tokens->emplace_back(string_view{match.first, static_cast<size_t>(match.length())});
+            // TODO: make more generic it work with string. string_view
+            const auto& match = (*it)[0];
+            tokens.emplace_back(SubToken::value_type{match.first, static_cast<size_t>(match.length())});
         }
 
-        return AlphanumericTokenT<_Rev>{std::move(tokens)};
+        return tokens;
     }
 };
-
-//
-// To string conversion
-//
-
-template<bool _Rev> std::string to_string(const NumericTokenT<_Rev>& other)
-{
-    return std::to_string(*other.Get());
-}
-
-template<bool _Rev> std::string to_string(const AlphanumericTokenT<_Rev>& other)
-{
-    std::string str;
-    for(const auto& sub_token : *other.Get() )
-    {
-        str += sub_token.s.to_string();
-    }
-    return str;
-}
-
-template<typename _Typ> std::ostream& operator<<(std::ostream& os, const TokenData<_Typ>& other)
-{
-    return (os << other.Get());
-}
-
-inline std::ostream& operator<<(std::ostream& os, const AlphanumericValue& other)
-{
-    for(const auto& sub_token : other )
-    {
-        os << sub_token.s.to_string();
-    }
-    return os;
-}
 
 //
 // User defined literals
 //
 
-inline NumericToken operator"" _nt(const char* v, size_t s)
+inline NumericToken operator"" _nt(const char* v, size_t s) { return Factory<NumericToken>::Create(string_view{v, s}); }
+inline AlphanumericToken operator"" _ant(const char* v, size_t s)
 {
-    return Factory<NumericToken>::Create(string_view{v, s});
+    return Factory<AlphanumericToken>::Create(string_view{v, s});
 }
 
-inline AlphanumericSubToken operator "" _st(const char* v, size_t s)
+namespace rez_impl
 {
-    return AlphanumericSubToken{string_view{v, s}};
-}
 
-inline AlphanumericToken operator"" _at(const char* v, size_t s)
-{
-    return Factory<AlphanumericToken>::Create(string_view{v,s});
-}
+inline SubToken operator"" _st(const char* v, size_t s) { return SubToken{SubToken::value_type{v, s}}; }
+
+} // namespace rez_impl
 
 #endif // REZ_TOKEN_HPP
